894c968b56ab04ccc25c87133475806ecf73990a
Kendall Brown <kndllbrwn1914@gmail.com>
2015-11-14 16:26:03 +0000

    Fixed error in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,10 +6,9 @@ class ContactsController < ApplicationController
   def create
       @contact = Contact.new(contact_params)
       if @contact.save
-          if @contact.save
-            name = params[:contact][:name]
-            email = params[:contact][:email]
-            body = params[:contact][:comments]
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
             
         ContactMailer.contact_email(name, email, body).deliver    
         flash[:success] = 'Message sent.'
@@ -17,12 +16,11 @@ class ContactsController < ApplicationController
       else
         flash[:danger] = 'Error occured, message has not been sent.'
         redirect_to new_contact_path
-          end
       end
+  end
   
     private
         def contact_params
          params.require(:contact).permit(:name, :email, :comments)
         end
-  
-  end
+end
